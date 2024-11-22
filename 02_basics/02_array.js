c819c1c8d8b5ff81b581022641b197d34dbafe6f
txrunishere <tsoni9286@gmail.com>
2024-11-22 21:00:30 +0530

    add some more array functions

---
+++ 02_basics/02_array.js
@@ -0,0 +1,11 @@
+const heroes = ["shaktiman", "ironman", "spiderman"];
+const characters = ["mario", "jett", "nzaru"];
+
+// heroes.push(characters);
+
+// console.log(heroes);
+// console.log(heroes[3][1]); // Here 3 represents the characters array and 1 represents the item on 1st index which is 'jett'
+
+const allArr = heroes.concat(characters); // It conbimes the two array 
+console.log(allArr);
+
