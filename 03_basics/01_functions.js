5622993f45c5cc10931003764d5f9a4ce23923ef
txrunishere <tsoni9286@gmail.com>
2024-11-24 12:41:04 +0530

    array and objects with function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,4 +29,48 @@ function loggedIn (userName = "temp") {
     return `${userName} just log in`
 }
 
-console.log(loggedIn());  // here, we console log the fnction because in function we return the statement
+// console.log(loggedIn());  // here, we console log the fnction because in function we return the statement
+
+
+// -+-+-+-+-+-+-+-+-+-+ rest operator -+-+-+-+-+-+-+-+-+-+
+
+function cartItemPrice(value1, value2, ...num) {
+    // return value1;
+    // return value2;
+    return num;
+}
+
+// console.log(cartItemPrice(35, 45, 55, 65, 75)); // Here if we want to print all these arguments then we use rest operator like ...num It return array of all arguments
+
+
+
+// *********************** function with objects ***********************
+
+const item = {
+    itemName: "book",
+    price: 199
+}
+
+function handleObject(object) { // Here in this parameter we pass our object in which we perform our task
+    console.log(`The Price of ${object.itemName} is ${object.price}`);
+}
+
+// handleObject(item);
+
+// handleObject({
+//     itemName: "Bottle",
+//     price: 99
+// }); // We also use it like that
+
+
+
+// functions with array
+
+const myArray = [100, 200, 300, 400, 500];
+
+function returnRandomValue(array) {
+    const rand = Math.floor(Math.random() * 5);
+    return array[rand]
+}
+
+console.log(returnRandomValue(myArray));  //returns the random value form the myArray
