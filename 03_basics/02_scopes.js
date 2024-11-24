2602a254e83a308610968a6bb66f6e0822ac9117
txrunishere <tsoni9286@gmail.com>
2024-11-24 15:02:57 +0530

    update scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,3 +9,49 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+ Scope Level and mini-hoisting +-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+function firstFunc() {
+    const name = "tarun";
+
+    function secondFunc() {
+        const myId = 6969;
+        console.log(`Hey, my name is ${name}`);
+    }
+
+    // console.log(myId); // Here, we don't access the myId because it is inside the secondFunc means myId is in a block scope here.
+    secondFunc();
+
+}
+// firstFunc()
+
+
+if (true) {
+    const user = "tarun";
+    if (user === "tarun") {
+        const id = 6900;
+        // console.log(`${user}'s id is ${id}`);
+    }
+    // console.log(id);
+}
+// console.log(user);
+
+
+// ---------------------------------- hoisting ----------------------------------
+
+
+
+// console.log(addOne(69)); // It executes the function and returns 70 in console
+
+function addOne(num) {
+    return num + 1;
+}
+
+
+// console.log(addTwo(68)); It gives a error because the declaration of function is after the execution 
+
+const addTwo = function (num) {
+    return num + 2;
+}
