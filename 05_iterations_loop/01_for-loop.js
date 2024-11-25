d98a05d18ee3716ce149aef52fe5e57df11284b4
txrunishere <tsoni9286@gmail.com>
2024-11-25 18:41:25 +0530

    update for loop

--- 05_iterations_loop/01_for-loop.js
+++ 05_iterations_loop/01_for-loop.js
@@ -1,5 +1,56 @@
 // for loop
 
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
-}
+// for (let index = 1; index <= 10; index++) {
+//     console.log(index);
+//     if (index == 6) {
+//         console.log("Hey, I am six");
+//     }
+// }
+
+
+// This loop is print 5 number for 10 times
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j <= 5; j++) {
+//         console.log(`Inner loop value is ${j} and outer is ${i}`);
+//     }
+// }
+
+
+// Prints table from 1 to 10
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`\nTable of: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value is ${j} and outer is ${i}`);
+//         console.log(i + ' * ' + j + ' = ' + i*j);
+//     }
+// }
+
+
+// Loop with Array
+
+// const arr = ["Bhalu", "Sherrr", "Bakri (GOAT)"];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+// break and continue
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`Lucky Number Detected ${i}`);
+//         break;
+//     }
+//     console.log(`The value of i is ${i}`);
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`Lucky Number Detected ${i}`);
+//         continue;
+//     }
+//     console.log(`The value of i is ${i}`);
+// }
+
