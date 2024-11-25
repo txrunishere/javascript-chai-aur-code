3c69a487568cb24f6649fc71c68a7e929cab2241
txrunishere <tsoni9286@gmail.com>
2024-11-25 19:40:05 +0530

    while loop

---
+++ 05_iterations_loop/02_while-loop.js
@@ -0,0 +1,20 @@
+// In while loop we declare our variable first
+
+let i = 1;
+
+// After that we declare our while loop and giving a condition in it. like., 
+
+while (i <= 10) {
+    console.log(i);
+    i++ // And at last we tell what we do increment in condition or decrement in condition
+}
+
+
+// while loop with array
+let arr = ["Bhalu", "Sherrrr", "Chitaaaaa", "Bakri", "Cowww"];
+let j = 0;
+while (j < arr.length) {
+    console.log(arr[j]);
+    j++;
+}
+
