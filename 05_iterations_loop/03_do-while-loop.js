b65d9fde83994d88709174b3bc4cb81dd9f8fb78
txrunishere <tsoni9286@gmail.com>
2024-11-25 19:54:38 +0530

    do while loop

---
+++ 05_iterations_loop/03_do-while-loop.js
@@ -0,0 +1,20 @@
+// In do-while loop the given condition was print 1 time every time you run the code. In do-while loop the condition of the loop was check at the last
+
+
+// Prints table of 5
+// let num = 1;
+// let tableNum = 5;
+
+// do {
+//     console.log(`${tableNum} * ${num} = ${tableNum * num}`);
+//     num++;
+// } while (num <= 10);
+
+
+
+// If we declare 11 in num2 variable. Here it don't match the condition, here it returns the value 11 and stop the execution of loop...
+let num2 = 11;
+
+do {
+    console.log(`Value is ${num2}`);
+} while (num2 <= 10);
